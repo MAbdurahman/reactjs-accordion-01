c04da0736637aba0387eac51c284c8645ab74ed7
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-17 05:24:56 -0500

    Correct components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,11 +4,12 @@ import AccordionItem from "./AccordionItem";
 export default function Accordion({data}) {
 
     return (
-            <section className="accordion">
-                <h2 className="u-text-center">Frequent Ask Questions</h2>
-                {data.map((el, i) => (
-                    <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
-                ))}
-            </section>
+        <section className="accordion">
+            <h2 className="u-text-center">Frequent Ask Questions</h2>
+            {data.map((el, i) => (
+                <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+            ))}
+        </section>
     );
 };
+
